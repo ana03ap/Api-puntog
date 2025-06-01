c48f28096bb6832039ca707155cc7ad50ea089b5
paviag <98894987+paviag@users.noreply.github.com>
2025-05-31 21:51:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -351,13 +351,15 @@ app.put("/categories/:id", async (req, res) => {
 // delete category
 app.delete("/categories/:id", async (req, res) => {
   try {
-    const result = await Category.findByIdAndDelete(req.params.id);
-    if (!result) {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
       return res.status(404).json({ error: "Category not found" });
     }
-
-    // TODO: await increment categories version?
-
+    const events = await Event.find({ type: category.type });
+    if (events) {
+      return res.status(404).json({ error: "Cannot delete category with events. Move events to different categories before deleting." });
+    }
+    await Category.findByIdAndDelete(req.params.id);
     res.status(200).json({ message: "Category deleted successfully" });
   } catch (error) {
     res
